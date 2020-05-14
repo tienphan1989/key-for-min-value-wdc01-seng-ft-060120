1c1d2a87861cc443dd88ece7ee6c9f4ed38d2c80
Tien Phan <tienphan1989@gmail.com>
2020-05-14 05:43:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   return nil if name_hash.size == 0
-  min_key = name_hash.first
+  min_key, min_value = name_hash.first
   min_value = name_hash.first
 name_hash.each do |key, value|
   if value < min_value
